e94d12533ad7772ee8250055adf8c3e36d8fd258
Cris Rocha <crisrocha.dev@gmail.com>
2022-07-05 10:30:47 -0300

    fix:alterando credenciais

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express, { application, json } from 'express';
 import AuthMiddleware from './middleware/AuthMiddleware.js';
-import ConfigAuthMiddleware from './middleware/ConfigAuthMiddleware.js';
 const app = express();
 import router from './routes.js';
 import router_config from './routes_config.js';
@@ -9,7 +8,6 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.json());
 
-app.use('/config/:key',ConfigAuthMiddleware);
 app.use(router_config);
 
 app.use('/:key',AuthMiddleware)
